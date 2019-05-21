ef51c69b4cc9cd8100b2a6a732bf063d57c3d37a
Hanna Lindgren <han.lind@gmail.com>
2019-05-21 16:24:18 +0200

    i18n_test.go

---
+++ i18n/i18n_test.go
@@ -0,0 +1,28 @@
+package i18n
+
+import (
+	"testing"
+)
+
+var fs = "Expected '%v', got '%v'"
+
+func Test_I18N(t *testing.T) {
+	i18n := I18N{
+		"Logged in":                     "Inloggad",
+		"Logged in as user %s":          "Inloggad som användare %s",
+		"Logged in as user %s, role %s": "Inloggad som användare %s, roll %s",
+	}
+
+	if exp, got := "Inloggad", i18n.S("Logged in"); exp != got {
+		t.Errorf(fs, exp, got)
+	}
+
+	// if exp, got := "Inloggad som användare hanna", i18n.S("Logged in as user %s", "hanna"); exp != got {
+	// 	t.Errorf(fs, exp, got)
+	// }
+
+	// if exp, got := "Inloggad som användare hanna, roll admin", i18n.S("Logged in as user %s, role %s", "hanna", "admin"); exp != got {
+	// 	t.Errorf(fs, exp, got)
+	// }
+
+}
