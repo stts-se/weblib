00edb3229b09ed4df3bfe962c978985820477f2c
Hanna Lindgren <han.lind@gmail.com>
2019-05-21 18:38:01 +0200

    doc

--- demoserver/demoserver.go
+++ demoserver/demoserver.go
@@ -1,4 +1,4 @@
-// A simple demo server , using the available libraries in this repository. It is also meant to show some design patterns, not suitable for library implementation, but still useful as templates for a server implementation.
+// A simple demo server, using the available libraries in this repository. It is also meant to show some design patterns, not suitable for library implementation, but still useful as templates for a server implementation.
 package main
 
 import (
